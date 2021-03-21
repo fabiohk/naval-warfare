6ab50920c9470fadcb5bb1e40ba841c75218b4cf
Fábio Kubo <elastic255@gmail.com>
2021-03-21 00:18:33 -0300

    feat: add first migration version with alembic

--- naval_warfare/db_models.py
+++ naval_warfare/db_models.py
@@ -2,7 +2,6 @@ import databases
 from sqlalchemy import Column
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
-from sqlalchemy import MetaData
 from sqlalchemy import String
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -10,7 +9,7 @@ from sqlalchemy.orm import backref
 from sqlalchemy.orm import relationship
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres"
 
 database = databases.Database(DATABASE_URL)
 
@@ -20,45 +19,55 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 
-class StandardMixin(Base):
+class StandardMixin:
     id = Column("id", Integer, primary_key=True, index=True, autoincrement=True)
 
 
-class Game(StandardMixin):
+class Game(StandardMixin, Base):
     __tablename__ = "games"
 
     settings_id = Column("settings_id", Integer, ForeignKey("game_settings.id"))
     settings = relationship("GameSettings", back_populates="games")
 
-    players = relationship("Player", secondary="matches")
+    players = relationship("Player", secondary="boards")
 
 
-class Player(StandardMixin):
+class Player(StandardMixin, Base):
     __tablename__ = "players"
 
     name = Column("name", String(255), unique=True, index=True)
 
-    games = relationship("Game", secondary="matches")
+    games = relationship("Game", secondary="boards")
 
 
-class GameMatch(StandardMixin):
-    __tablename__ = "game_match"
+class Board(StandardMixin, Base):
+    __tablename__ = "boards"
+
+    length = Column("length", Integer)
+    width = Column("width", Integer)
 
     game_id = Column("game_id", Integer, ForeignKey("games.id"))
     player_id = Column("player_id", Integer, ForeignKey("players.id"))
 
-    game = relationship(Game, backref=backref("matches"))
-    player = relationship(Player, backref=backref("matches"))
+    game = relationship(Game, backref=backref("boards"))
+    player = relationship(Player, backref=backref("boards"))
 
 
-class Ship(StandardMixin):
+class ShipPosition(StandardMixin, Base):
+    __tablename__ = "ships_position"
+
+    direction = Column("direction", String(255))
+    cartesian_position = Column("cartesian_position", Integer)
+
+
+class Ship(StandardMixin, Base):
     __tablename__ = "ships"
 
     kind = Column("kind", String(255), unique=True)
     length = Column("length", Integer)
 
 
-class GameSettings(StandardMixin):
+class GameSettings(StandardMixin, Base):
     __tablename__ = "game_settings"
 
     games = relationship("Game", back_populates="settings")
